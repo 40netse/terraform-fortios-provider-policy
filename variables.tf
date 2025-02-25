151a23c8c3cc112f49ef8882f47046ee11b40b5b
Robert Reris <rreris@fortinet.com>
2025-02-25 15:18:37 -0800

    adding variable defaults

--- variables.tf
+++ variables.tf
@@ -1,10 +1,13 @@
 variable "aws_region" {
   description = "The AWS region to use"
+  default     = "us-east-1"
 }
 variable "cp" {
   description = "Customer Prefix to apply to all resources"
+  default     = "test"
 }
 variable "env" {
   description = "The Tag Environment to differentiate prod/test/dev"
+  default     = "test"
 }
 
